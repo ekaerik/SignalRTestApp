ea245f5443fd1755ac1b50b0f00d0cd56f53edc6
unknown <eriks@TELEOPTI699.toptinet.teleopti.com>
2014-02-03 12:22:24 +0100

    first checkin

---
+++ SignalRBroker/RandomHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SignalRBroker
+{
+	[HubName("random")]
+	public class RandomHub : Hub
+	{
+		public void Do(string what)
+		{
+			Clients.Caller.say(what);
+		}
+	}
+}
---
+++ SignalRTestApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+
+namespace SignalRTestApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Connecting");
+			var connection = new HubConnection("http://localhost/SignalRBroker");
+			connection.StateChanged += change => Console.WriteLine(Environment.NewLine + change.OldState + " => " + change.NewState);
+			connection.Closed +=() =>
+				{
+					Console.WriteLine("Closed event triggered");
+					Thread.Sleep(TimeSpan.FromSeconds(5));
+					// BUG? If you use .Wait() it will not reconnect
+					connection.Start().Wait();
+					//connection.Start();
+					Console.WriteLine("Connection is starting / Connection have started");
+				};
+			var proxy = connection.CreateHubProxy("random");
+			connection.Start().Wait();
+
+			Console.WriteLine("Start sending");
+			for (var i = 0; i < 100000000; i++)
+			{
+				try
+				{
+					while (connection.State != ConnectionState.Connected)
+					{
+					}
+					proxy.Invoke("do", "A simple string message");
+					Console.Write(".");
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message);
+				}
+			}
+			connection.Stop();
+			Console.WriteLine("Done!");
+			Console.ReadLine();
+		}
+	}
+}
